f64e03391fffc139b68b00cf1f81ea9bf4f67a0d
Diego Lechuga <diegolechuga2003@gmail.comgit config --list>
2025-02-10 20:40:08 -0600

    first commit

---
+++ 1.1.1-TiposDeDatos/main.js
@@ -0,0 +1,29 @@
+//Este es mi primer script en JavaScript
+
+console.log(5);
+console.log(typeof 5);
+
+
+console.log("Hola Mundo");
+console.log(typeof "Hola Mundo");
+
+
+/* Tipos de datos en JavaScript
+Es importante conocerlos */
+
+console.log(true);
+console.log(typeof true);
+
+console.log(5 + "5");
+
+let nombre = "Diego";
+
+console.log("Hola " + nombre + " ¿Cómo estás?");
+
+let numeroEspecial = 5;
+
+let numeroEspecialTexto = "5";
+
+console.log(numeroEspecial + parseInt(numeroEspecialTexto));
+
+let textoAleatorio = "Hola Mundo";
